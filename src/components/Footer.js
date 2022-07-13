30043fe29c904126130b38226418bd16997a989c
umairkhalid <umairkhalid@fastmail.fm>
2022-07-13 22:52:12 +1000

    Stylesheet added

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Icons from "../components/icons/Icons";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <Icons />
+    </footer>
+  );
+}
+
+export default Footer;
