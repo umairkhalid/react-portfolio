bdbcc4d4c9f7543889d4b053daf8bfbf75183d85
umairkhalid <umairkhalid@fastmail.fm>
2022-07-16 19:18:58 +1000

    Suspense lazy code splitting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
-import PortfolioContainer from './components/PortfolioContainer';
+import { lazy, Suspense } from 'react';
 
-const App = () => <PortfolioContainer />;
+const PortfolioContainer = lazy(() => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(import('./components/PortfolioContainer')), 2000);
+  });
+});
+
+const App = () => {
+  return (
+    <div className="App">
+      <Suspense fallback={<div>Loading...</div>}>
+        <PortfolioContainer />
+      </Suspense>
+    </div>
+  );
+};
 
 export default App;
